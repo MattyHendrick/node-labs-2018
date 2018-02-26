223c8f268175a7f32a07b3ecd706c15cf707873b
Matthew Hendrick <matthewhendrick73@gmail.com>
2018-02-26 16:17:32 +0000

    added routing for add, update, delete contact

--- node-lab2/api/contacts/index.js
+++ node-lab2/api/contacts/index.js
@@ -8,24 +8,26 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-	let newContact = req.body;
-	if (newContact) {
-		contacts.push({name: newContact.name, address: newContact.address, phone_number: newContact.phone_number});
-		res.status(201).send({message: "Contact Created"});
-	} else {
-		res.status(400).send({message: "Unable to find Contact in request. No contact found in body"});
-	}
+        let newContact = req.body;
+        if (newContact) {
+          contacts.push({name: newContact.name, address: newContact.address,
+          phone_number: newContact.phone_number});
+          res.status(201).send({message: 'Contact Created'});
+      } else {
+            res.status(400).send({message:
+            'Unable to find Contact in request. No Contact Found in body'});
+      }
 });
 
-//Update Contact
+// Update a contact
 router.put('/:id', (req, res) => {
-	const key = req.params.id;
-	const updateContact = req.body;
-	const index = contacts.map((contact) => {
-		return contact.phone_number;
-	}).indexOf(key);
-		if (index !== -1) {
-			contacts.splice(index, 1, {name: updateContact.name, address: updateContact.address,
+     const key = req.params.id;
+     const updateContact = req.body;
+     const index = contacts.map((contact)=>{
+return contact.phone_number;
+}).indexOf(key);
+            if (index !== -1) {
+               contacts.splice(index, 1, {name: updateContact.name, address: updateContact.address,
                phone_number: updateContact.phone_number});
                res.status(200).send({message: 'Contact Updated'});
               } else {
@@ -41,9 +43,9 @@ return contact.phone_number;
 }).indexOf(key);
     if (index > -1) {
 contacts.splice(index, 1);
-        res.status(200).send({message: 'Deleted contact with phone_number: ${key}.'});
+        res.status(200).send({message: `Deleted contact with phone_number: ${key} `});
     } else {
-      res.status(400).send({message: 'Unable to find contact with phone_number: ${key}.'});
+      res.status(400).send({message: `Unable to find Contact with phone_number: ${key}. No contact Deleted`});
       }
 });
 
